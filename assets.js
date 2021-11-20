8bc7de075e88a060dd78f2601e179af7b9957573
alondra-ortega <92123260+alondra-ortega@users.noreply.github.com>
2021-11-20 13:28:15 -0500

    added a doc w questions

---
+++ assets.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      title: "What is the best loop to use for iterating through a list?",
+      choices: ["if else", "when", "for", "next"],
+      answer: "for"
+    },
+    {
+      title: "What indicates a variable is an array?",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "square brackets"
+    },
+    {
+      title: "How can you select a specific node through javascript?",
+      choices: ["using a parent and child", "looping through the html", "using a variable", "making the nodes into an array"],
+      answer: "using a parent and child"
+    },
+    {
+      title: "How can I minimize the CSS I type",
+      choices: ["jQuery", "Bootstrap", "do not format the website", "have less variables"],
+      answer: "Bootstrap"
+    },
+    {
+      title: "How can I ensure a variable does not change",
+      choices: ["declare as const", "var==", "leave it alone", "create it in a separate document"],
+      answer: "declare as const"
+    },
+  ];
